52cd69d4ef37de37fd57d15f40a7104b67be42e8
annie-tu <annietu18@gmail.com>
2021-02-17 18:01:44 -0800

    update

--- bargraph.R
+++ bargraph.R
@@ -1,1 +1,14 @@
 # Bar graph of deaths by age group in King County
+
+library(tidyverse)
+age_data <- read.csv(paste(getwd(), "/data/nts-by-date-all-demographics-jan-19.csv", sep = ""))
+
+columns <- age_data %>% 
+  select(Deaths, Age_Group) %>% 
+  gather(key = Deaths, value = Population, -Age_Group)
+
+ggplot(columns, aes (x = Age_Group, y = Population, fill = condition) +
+# geom_col(mapping = aes(x = Age_Group, y = Population, color = "Deaths"))
+  geom_bar(position = "stack", stat = "identity")
+                 
+ 
