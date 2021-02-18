7b42d64ee1ef8c4f0376d018792bcf420b30eab9
annie-tu <annietu18@gmail.com>
2021-02-17 20:06:54 -0800

    graph

--- bargraph.R
+++ bargraph.R
@@ -3,12 +3,21 @@
 library(tidyverse)
 age_data <- read.csv(paste(getwd(), "/data/nts-by-date-all-demographics-jan-19.csv", sep = ""))
 
-columns <- age_data %>% 
-  select(Deaths, Age_Group) %>% 
-  gather(key = Deaths, value = Population, -Age_Group)
+age_data <- age_data %>% 
+  group_by(Age_Group) %>% 
+  mutate(Total = Population - Deaths)
 
-ggplot(columns, aes (x = Age_Group, y = Population, fill = condition) +
-# geom_col(mapping = aes(x = Age_Group, y = Population, color = "Deaths"))
-  geom_bar(position = "stack", stat = "identity")
+columns <- age_data %>% 
+  select(Age_Group, Deaths, Total, Population) %>% 
+  gather(key = Deaths, value = Population, -Age_Group, -Population)
+
+
+ggplot(columns) +
+  geom_col(mapping = aes(x = Age_Group, y = Population, fill = Deaths))
+  #geom_col(aes(fill = Deaths))
+  
+#  geom_col(mapping = aes(x = Age_Group, y = Population, fill = "Deaths"))
+  #geom_bar(position = "stack", stat = "identity")
+# , aes (x = Age_Group, y = Population, fill = Population)
                  
  
