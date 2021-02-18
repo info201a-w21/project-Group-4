2f3854fadb09e1aa12446302f8b2c819e785edc9
annie-tu <annietu18@gmail.com>
2021-02-17 20:14:51 -0800

    add graph

--- bargraph.R
+++ bargraph.R
@@ -11,13 +11,8 @@ columns <- age_data %>%
   select(Age_Group, Deaths, Total, Population) %>% 
   gather(key = Deaths, value = Population, -Age_Group, -Population)
 
-
 ggplot(columns) +
   geom_col(mapping = aes(x = Age_Group, y = Population, fill = Deaths))
-  #geom_col(aes(fill = Deaths))
-  
-#  geom_col(mapping = aes(x = Age_Group, y = Population, fill = "Deaths"))
-  #geom_bar(position = "stack", stat = "identity")
-# , aes (x = Age_Group, y = Population, fill = Population)
-                 
- 
+
+ggplot(age_data) +
+  geom_col(mapping = aes(x = Age_Group, y = Deaths))
