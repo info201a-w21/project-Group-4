85819359d23dc68ecb3b2ed3807ea12a201ecf8b
chnguyen22 <nchristine101@gmail.com>
2021-02-19 18:36:22 -0800

    Finalize Summary

--- Summary_List.R
+++ Summary_List.R
@@ -7,11 +7,9 @@ summary_info <- list(combined_data)
 summary_info$num_cities <- nrow(combined_data)
 summary_info$some_min_value <- min_emergency_fs_city
 
-
 summary_info$highest_death_cities <- combined_data %>%
   filter(Deaths == min(Deaths, na.rm = T)) %>%
   pull(Location_Name)
-
 
 summary_info$highest_deaths_fs <- combined_data %>%
   filter(deaths_per_fs == max(deaths_per_fs)) %>%
